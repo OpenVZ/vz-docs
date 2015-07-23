03c9d676335eb39521eed5918b9dc90512445286
Artem Pavlenko <apavlenko@odin.com>
2015-07-23 15:09:32 +0300

    Reconfigured webhelp to use saxon6.5.5 and xerces-j2 from standard repositories

--- webhelp.sh
+++ webhelp.sh
@@ -3,9 +3,9 @@ a2x -vL -a docinfo1 -r images -f docbook $1.asc
 rm -f build.xml
 cat <<_EOF_ >build.xml
 <project>
-  <property name="xslt-processor-classpath" value="saxon6-5-5/saxon.jar"/>
-  <property name="xercesImpl.jar" value="xerces-2_11_0/xercesImpl.jar"/>
-  <property name="xml-apis.jar" value="xerces-2_11_0/xml-apis.jar"/>
+  <property name="xslt-processor-classpath" value="/usr/share/java/saxon.jar"/>
+  <property name="xercesImpl.jar" value="/usr/share/java/xerces-j2.jar"/>
+  <property name="xml-apis.jar" value="/usr/share/java/xml-commons-apis.jar"/>
   <property name="input-xml" value="$1.xml"/>
   <property name="output-dir" value="$1.webhelp"/>
   <property name="stylesheet-path" value="stylesheets/virtuozzo.webhelp.xsl"/>
